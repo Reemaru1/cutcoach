bb48fac9cea0b9afd40259e9e48d7e40a8f3020c
Reem <127992884+Reemaru1@users.noreply.github.com>
2026-07-15 12:50:01 +0200

    Bump visible app version to 5.5

--- upgrade-340.js
+++ upgrade-340.js
@@ -1,6 +1,6 @@
 'use strict';
 (function(){
-  const VERSION='5.4.0';
+  const VERSION='5.5.0';
   const LIB='cutcoach_library_v1';
   const OVR='cutcoach_library_overrides_v1';
   const WATER='cutcoach_water_v1';
