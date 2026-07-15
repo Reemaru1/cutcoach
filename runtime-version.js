c4850b1b566b0d4dbe80da11452ebf45c395ddd0
Reem <127992884+Reemaru1@users.noreply.github.com>
2026-07-15 20:03:38 +0200

    Set app version to 6.0.2

--- runtime-version.js
+++ runtime-version.js
@@ -1,6 +1,6 @@
 'use strict';
 (function(){
-  const VERSION='6.0.1';
+  const VERSION='6.0.2';
   function applyVersion(){
     const node=document.querySelector('#appVersion');
     if(node)node.textContent=`Version ${VERSION}`;
