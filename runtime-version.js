3d1b5ded032ca4d4dd3030f0fa3154a138712f71
Reem <127992884+Reemaru1@users.noreply.github.com>
2026-07-15 23:13:30 +0200

    Set visible version to 6.0.9

--- runtime-version.js
+++ runtime-version.js
@@ -1,6 +1,6 @@
 'use strict';
 (function(){
-  const VERSION='6.0.8';
+  const VERSION='6.0.9';
   function applyVersion(){
     const node=document.querySelector('#appVersion');
     if(node)node.textContent=`Version ${VERSION}`;
