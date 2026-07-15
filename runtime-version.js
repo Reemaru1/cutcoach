f05c684969d592349e7f28c9ee70996e6a21cc17
Reem <127992884+Reemaru1@users.noreply.github.com>
2026-07-15 23:48:55 +0200

    Set CutCoach version 6.0.11

--- runtime-version.js
+++ runtime-version.js
@@ -1,6 +1,6 @@
 'use strict';
 (function(){
-  const VERSION='6.0.10';
+  const VERSION='6.0.11';
   function applyVersion(){
     const node=document.querySelector('#appVersion');
     if(node)node.textContent=`Version ${VERSION}`;
