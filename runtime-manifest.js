903413dc5e6348123eff7451ff82bddae547bed4
Reem <127992884+Reemaru1@users.noreply.github.com>
2026-07-17 14:03:53 +0200

    chore: cache CutCoach 7.3.1 cleanup

--- runtime-manifest.js
+++ runtime-manifest.js
@@ -1,13 +1,13 @@
 'use strict';
 self.CUTCOACH_RUNTIME=Object.freeze({
-  version:'7.3.0',
+  version:'7.3.1',
   baseAssets:Object.freeze([
     './style.css?v=2.3.0','./core.js?v=7.0.0','./render.js?v=7.0.0','./actions.js?v=7.0.0','./app.js?v=7.0.0','./manifest.webmanifest?v=2.3.0','./icon.svg','./apple-touch-icon.png?v=2.3.0','./icon-192.png?v=2.3.0','./library.css?v=7.0.0'
   ]),
   styles:Object.freeze([
-    './upgrade-360.css?v=4.2.0','./upgrade-420.css?v=4.2.0','./upgrade-430.css?v=4.3.0','./upgrade-560.css?v=5.6.0','./upgrade-561.css?v=5.6.1','./upgrade-562.css?v=5.6.2','./upgrade-563.css?v=5.6.3','./upgrade-564.css?v=5.6.4','./upgrade-565.css?v=5.6.5','./upgrade-570.css?v=5.7.0','./nutrition.css?v=7.0.0','./journal-date.css?v=6.2.2','./journal-ui.css?v=6.2.3','./water-animation.css?v=6.8.5','./nutrition-v7.css?v=7.0.0','./ui-effects-v7.css?v=7.1.0','./journal-v72.css?v=7.2.0','./nutrition-v73.css?v=7.3.0'
+    './upgrade-360.css?v=4.2.0','./upgrade-420.css?v=4.2.0','./upgrade-430.css?v=4.3.0','./upgrade-560.css?v=5.6.0','./upgrade-561.css?v=5.6.1','./upgrade-562.css?v=5.6.2','./upgrade-563.css?v=5.6.3','./upgrade-564.css?v=5.6.4','./upgrade-565.css?v=5.6.5','./upgrade-570.css?v=5.7.0','./nutrition.css?v=7.0.0','./journal-date.css?v=6.2.2','./journal-ui.css?v=6.2.3','./water-animation.css?v=6.8.5','./nutrition-v7.css?v=7.0.0','./ui-effects-v7.css?v=7.1.0','./journal-v72.css?v=7.2.0','./nutrition-v73.css?v=7.3.1'
   ]),
   scripts:Object.freeze([
-    './food-catalog.js?v=7.0.0','./everyday-catalog-v73.js?v=7.3.0','./library.js?v=7.0.0','./library-init.js?v=7.0.0','./scanner-v2.js?v=7.0.0','./off-lookup.js?v=7.0.0','./upgrade-340.js?v=7.0.0','./nutrition.js?v=7.0.0','./journal.js?v=7.0.0','./water-animation.js?v=6.8.5','./nutrition-v7.js?v=7.0.0','./nutrition-v73.js?v=7.3.0','./ui-effects-v7.js?v=7.1.0','./journal-v72.js?v=7.2.0','./version-v7.js?v=7.1.0'
+    './food-catalog.js?v=7.0.0','./everyday-catalog-v73.js?v=7.3.0','./library.js?v=7.0.0','./library-init.js?v=7.0.0','./scanner-v2.js?v=7.0.0','./off-lookup.js?v=7.0.0','./upgrade-340.js?v=7.0.0','./nutrition.js?v=7.0.0','./journal.js?v=7.0.0','./water-animation.js?v=6.8.5','./nutrition-v7.js?v=7.0.0','./nutrition-v73.js?v=7.3.1','./ui-effects-v7.js?v=7.1.0','./journal-v72.js?v=7.2.0','./version-v7.js?v=7.3.1'
   ])
 });
